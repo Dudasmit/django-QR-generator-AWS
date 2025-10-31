1edde79461b6223e8dfaef99a2d35084e0a4a31d
Ivan <Dudasmit@bigmir.net>
2025-10-31 15:13:54 +0100

    1

--- inriver_qr/settings.py
+++ inriver_qr/settings.py
@@ -136,7 +136,7 @@ SWAGGER_SETTINGS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -154,7 +154,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
